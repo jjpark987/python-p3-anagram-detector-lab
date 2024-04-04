c0ba0a12209da4eac24c337ab1e265ae2b05b6e2
Jason Park <jjpark987@gmail.com>
2024-04-04 09:40:09 -0700

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import ipdb
+
+class Anagram:
+    def __init__(self, string) -> None:
+        self.string = string
+
+    def match(self, strings_list):
+        results = []
+
+        self_string = sorted(list(self.string))
+
+        for string in strings_list:
+            if self_string == sorted(list(string)):
+                ipdb.set_trace()
+                results.append(string)
+            
+        return results
+        
+anagram = Anagram('enlist').match(['listen','teddy','alfred'])
